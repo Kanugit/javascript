c4ba5c12abe38a6307675d194392afd824e40cc1
Kanupriya Tiwari <128897513+Kanugit@users.noreply.github.com>
2024-10-22 17:37:18 +0000

    comparison and datatype Summary

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,14 @@
+//only for checking thing ,avoid this type of comparison always write clean and readable code
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null===0);
+console.log(null===undefined)
+console.log(null>=0);
+console.log(undefined>=0);
+
+// strict check
+
+console.log("2"===2);
