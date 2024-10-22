8bff20364041e01f08b2000f49eb20f0207ee584
Kanupriya Tiwari <128897513+Kanugit@users.noreply.github.com>
2024-10-22 12:28:27 +0000

    conversions and opreations

---
+++ 01_basic/03_conversionOpreation.js
@@ -0,0 +1,43 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let value= Number(score)
+console.log(typeof value);
+
+value=String(score)
+console.log(typeof value);
+
+let a='123abc'
+b=Number(a)
+console.log(typeof b)
+console.log(b)
+
+//"33"=>33
+//"123abc"=>NaN
+// true=>1, false=>0
+
+//**** opreations ***//
+ value =3
+ newvalue=-value
+ console.log(newvalue)
+ console.log(2**2) //power
+
+ let str1 ="hello"
+ let str2='meeee'
+ let str3=str1 + str2
+ console.log(str3)
+
+ console.log('123abc'+' '+'23')
+
+let n1= 12+'23';
+console.log(typeof n1);
+console.log(typeof('abc'+12));
+console.log(typeof(1+2+"3"))
+ 
+let x=3
+let y=x++
+console.table([x,y])
+
+ 
+
