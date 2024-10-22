538f7f01e4830041a3968ae6d5d56c052e50473c
Kanupriya Tiwari <128897513+Kanugit@users.noreply.github.com>
2024-10-22 10:27:11 +0000

    variables and datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=123
+let accountEmail='k1@google.com'
+var accountPass='1234'
+accountCity='Gwalior'
+
+console.table([accountId,accountEmail,accountPass,accountCity])
+ accountEmail='k2@google.com'
+ accountPass='234'
+ console.table([accountEmail,accountPass])
+ 
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) using nodejs not browser
+ 
+console.log(3
+    +3 //code readability should be high
+)
+
+console.log(3+3);
+
+let name="kanu"; //string
+let age=20   //number
+let islogged=false //boolean true/false
+
+//null standalone value   let state=null (empty)
+//undefined -> not assign value but declare variable 
+// symbol -> used mostly in react, used for uniqueness
+
+//object
+
+console.log(typeof age);
+console.log(typeof null) //output object
